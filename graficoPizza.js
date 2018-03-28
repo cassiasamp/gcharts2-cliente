1a2f8e4f7f6d4a2902999752cd2f2d3141024730
Cássia Sampaio <cassiasampaios@gmail.com>
2018-03-27 22:55:10 -0300

    atualizações e mudança de dataset.

--- graficoPizza.js
+++ graficoPizza.js
@@ -7,29 +7,55 @@ function desenharGraficoPizza(){
 	tabela.addColumn('number', 'valores');
 	//linhas da tabela
 		tabela.addRows([
-			['Educação',2000],
-			['Transporte',500],
-			['Lazer',230],
-			['Saúde',50],
-			['Cartão de crédito',900],
-			['Alimentação',260]
+			['Tecnologia', 47814],
+			['Marketing', 22552],
+			['Administrativo', 65243],
+			['Linhas de crédito', 55504],
+			['Segurança', 86085],
+			['Atendimento ao cliente', 12605]
 		]);	
+
+		tabela.sort({column: 1, asc: true});
+
 	//opcoes que customizam o grafico
 		var opcoes = {
-			title:'Tipos de Gastos',
+			title:'Gastos por unidade de negócio',
 			height: 400,
-			width: 800,
-			is3D: true,
-			legend: 'labeled',
+			width: 900,
+			legend: 
+				{
+					position: 'labeled', 
+					textStyle: 
+						{
+							color: '#928e8a', 
+							fontSize: 16
+						}
+				},
 			pieSliceText: 'value',
-			//colors:['grey', 'red', 'yellow', 'blue', 'pink', 'purple']
 			slices: 
 				{
-					1:{color:'grey'},
+					0:{color:'#a6a6a6'},
+					1:{color:'#a6a6a6'},
 					2:{color:'#a6a6a6'},
-					4:{offset: 0.4},
-					3:{color:'grey'},
-					5:{color:'grey'}
+					3:{color:'#a6a6a6'},
+					4:{color:'#1b9af5', offset: 0.15},
+					5:{color:'#f5921b', offset: 0.2}	
+				},
+			pieStartAngle: 4,
+			titleTextStyle: 
+				{ 
+					color: '#5e5851',
+					fontName: 'Arial',
+					fontSize: 20,
+					bold: true,
+					italic: false 
+				},
+			chartArea:
+				{
+					left: 80,
+					top: 100,
+					width:'90%',
+					height:'70%'
 				}
 			};
 	//desenhando grafico
