f3aefaeadc977e7fbf40d52f86a5a6a143d282bf
Cássia Sampaio <cassiasampaios@gmail.com>
2018-03-28 16:30:23 -0300

    mudança de dataset e refinamento

--- graficoLinha.js
+++ graficoLinha.js
@@ -7,13 +7,14 @@ function desenharGraficoLinha(){
 	tabela.addColumn('number', 'Conta salário');
 	tabela.addColumn('number', 'Conta corrente');
 	tabela.addColumn('number', 'Cartão de crédito');
+	tabela.addColumn('number', 'Conta de cartão');
 	//linhas da tabela
 		tabela.addRows([
-			['2012', 200, 275, 343, 470],
-			['2013', 221, 340, 380, 476],
-			['2014', 343, 331, 376, 450],
-			['2015', 442, 391, 387, 510],
-			['2016', 465, 400, 412, 532]
+			['2012', 200, 275, 343, 470, 322],
+			['2013', 221, 340, 380, 476, 346],
+			['2014', 343, 331, 376, 450, 351],
+			['2015', 442, 391, 387, 510, 357],
+			['2016', 465, 400, 412, 532, 360]
 		]);
 	//opcoes que customizam o grafico
 		var opcoes = {
@@ -31,7 +32,12 @@ function desenharGraficoLinha(){
 			legend: 
 				{
 					position: 'right', 
-					maxLines: 1
+					maxLines: 1,
+					textStyle: 
+						{
+							color: '#928e8a', 
+							fontSize: 16
+						}
 				},
 			chartArea:
 				{
@@ -46,7 +52,18 @@ function desenharGraficoLinha(){
 					fontName: 'Arial',
 					fontSize: 20,
 					bold: true 
-				}
+				},
+			series: {
+						0: { color: '#3682ff', lineWidth: 4 },
+						1: { color: '#a6a6a6' },
+						2: { color: '#a6a6a6' },
+						3: { color: '#a6a6a6' },
+						4: { color: '#a6a6a6' },
+						5: { color: '#a6a6a6' }
+				  },
+			vAxis: { 
+						maxValue: 560
+					}
 			}
 
 
